46a99ddaf7a54368f1ef77231d530ad5e7598b45
VF-ROOT\pilkm001 <MatthewRichard.Pilkington@vcontractor.co.za>
2021-06-10 16:22:46 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import About from "./pages/about/about";
-
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import GenerateHoroscopes from "./pages/generate_horoscope/generate_horoscope";
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/generate">
+          <GenerateHoroscopes />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
---
+++ src/pages/generate_horoscope/generate_horoscope.jsx
@@ -0,0 +1,3 @@
+export default function GenerateHoroscopes() {
+  return <div>Hello Generate Horoscopes</div>;
+}
