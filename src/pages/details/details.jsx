458ac996bb14f9b91640093584ed988ee5430aaf
ludevinparkinson@gmail.com <ludevinparkinson@gmail.com>
2021-06-17 11:39:35 +0200

    Updated Details Page

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,67 +1,69 @@
 import "./details.css";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
+const animatedComponents = makeAnimated();
 
 export default function Details() {
+  const hobbies = [
+    { value: "Fishing", label: "Fishing" },
+    { value: "Hunting", label: "Hunting" },
+  ];
+
+  const dinosaurs = [
+    { value: "Triceratops", label: "Triceratops" },
+    { value: "Tyrannosaurus", label: "Tyrannosaurus" },
+    { value: "Stegosaurus", label: "Stegosaurus" },
+    { value: "Brachiosaurus", label: "Brachiosaurus" },
+    { value: "Baryonyx", label: "Baryonyx" },
+    { value: "Ankylosaurus", label: "Ankylosaurus" },
+    { value: "Oviraptor", label: "Oviraptor" },
+  ];
+
+  const professions = [{ value: "Docter", label: "Doctor" }];
+
   return (
     <div className="horror_scope_container">
-      <h1>Your Details</h1>
+      <h1>My Details</h1>
 
       <form action="/dashboard">
         <table>
           <tr>
             <th>
-              <label>1. Enter your date of birth?</label>
+              <label>1. My date of birth?</label>
             </th>
             <td>
               <input type="date" />
             </td>
           </tr>
-
           <tr>
             <th>
               {" "}
-              <label>2. What is your favourite Dinosaur?</label>{" "}
+              <label>2. What is my favourite Dinosaur?</label>{" "}
             </th>
             <td>
-              {" "}
-              <select>
-                <option>Triceratops</option>
-                <option>Tyrannosaurus</option>
-                <option>Stegosaurus</option>
-                <option>Brachiosaurus</option>
-                <option>Baryonyx</option>
-                <option>Ankylosaurus</option>
-                <option>Oviraptor</option>
-              </select>
+              <Select options={dinosaurs} />
             </td>
           </tr>
 
           <tr>
             <th>
-              <label>3. What is your star sign?</label>
+              <label>3. What are my hobbies?</label>{" "}
             </th>
             <td>
-              <select>
-                <option></option>
-                <option>Metal tracking</option>
-              </select>
+              <Select
+                components={animatedComponents}
+                closeMenuOnSelect={false}
+                options={hobbies}
+                isMulti
+                className="select-input"
+              />
             </td>
           </tr>
 
           <tr>
             <th>
-              <label>4. What is your hobby?</label>{" "}
-            </th>
-            <td>
-              <select>
-                <option>Fishing</option>
-                <option>Metal tracking</option>
-              </select>
-            </td>
-          </tr>
-
-          <tr>
-            <th>
-              <label>5. I am the nth child of my family?</label>
+              <label>4. I am the nth child of my family?</label>
             </th>
             <td>
               <input type="number" placeholder="child number" />
@@ -70,13 +72,10 @@ export default function Details() {
 
           <tr>
             <th>
-              <label>6. My Profession</label>
+              <label>5. My Profession</label>
             </th>
             <td>
-              <select>
-                <option>Doctor</option>
-                <option>Game Design</option>
-              </select>
+              <Select options={professions} />
             </td>
           </tr>
         </table>
