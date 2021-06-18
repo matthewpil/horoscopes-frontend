57c2ba6813541622390c8f443b005e50dc0b257a
Gian Uys <gianuys@discovery.bank>
2021-06-18 03:15:08 +0200

    Reverted back to stable branch.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,34 +2,16 @@ import "./home.css";
 import { GoogleLogin } from "react-google-login";
 import { useHistory } from "react-router-dom";
 import { login } from "../../utils/auth/login";
-import { requests } from "../../repositories/request"
-
-const AUTH_ENDPOINT = `${process.env.REACT_APP_API_BASE}Auth/Google`;
-
 export default function Home() {
   const history = useHistory();
   const onLoginSuccess = (res) => {
-
-    requests.post(AUTH_ENDPOINT, {body: JSON.stringify({
-      tokenId: res.tokenId
-    }) }).then(authResponse => {
-      
-      if (!(authResponse.token === "")) {
-        login(res);
-        if (authResponse.registeredUser) {
-          history.push("/dashboard");
-        } else {
-          history.push("/details");
-        }
-      }
-    });
-
+    login(res);
+    history.push("/details");
   };
 
   const onLoginFail = (res) => {
     console.log("Login failed: ", res);
   };
-
   return (
     <div className="header">
       <h1>Welcome to Totally Legit Horoscopes.</h1>{" "}
